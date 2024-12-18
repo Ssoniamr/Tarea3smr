97f66a9510622e9876e3f9e213b43ce7a9fca623
Ssoniamr <sonia87mr@gmail.com>
2024-12-18 22:16:32 +0100

    añadido navegadores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,15 +4,15 @@ plugins {
 
 android {
     namespace = "dam.pmdm.tarea3smr"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dam.pmdm.tarea3smr"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
+        vectorDrawables.useSupportLibrary=true;
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -29,20 +29,26 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 
-
 dependencies {
-
+    implementation("androidx.navigation:navigation-ui:2.8.5")
+    implementation("androidx.navigation:navigation-fragment:2.8.5")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.media3:media3-common:1.5.0")
+    implementation("androidx.preference:preference:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.preference)
-    implementation(libs.media3.common)
+
 
 }
--- app/src/main/java/dam/pmdm/tarea3smr/MainActivity.java
+++ app/src/main/java/dam/pmdm/tarea3smr/MainActivity.java
@@ -1,20 +1,73 @@
 package dam.pmdm.tarea3smr;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import dam.pmdm.tarea3smr.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        //se infla el layoud usando viewBinding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //--------------CONFIGURACION DE NAVEGACION--------------
+
+        //obtenemos el contenedor de fragmentos.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        navHostFragment = (NavHostFragment) fragmentManager.findFragmentById(R.id.nav_host_fragment);
+
+        //si este no es nulo obetemos el controlador de navegacion y lo conectamos con el navigationButton
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(binding.navigationButton, navController);
+        }
+
+        binding.navigationButton.setOnItemSelectedListener(this::onNavigationItemSelected);
 
     }
+
+
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        int menuItemElegido = menuItem.getItemId();
+
+        if (menuItemElegido == R.id.preferences_ajustes) {
+            navController.navigate(R.id.preferenceAjustes);
+            return true;
+        } else if (menuItemElegido == R.id.fragment_lista_pokemons_disponibles) {
+            navController.navigate(R.id.fragment_lista_pokemons_disponibles);
+            return true;
+        } else if (menuItemElegido == R.id.fragment_lista_pokemons_capturados) {
+            navController.navigate(R.id.fragment_lista_pokemons_capturados);
+            return true;
+        }
+        return true;
+    }
+
 }
+
+
+
